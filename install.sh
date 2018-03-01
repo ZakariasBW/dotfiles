57b0b5273137366405cccc37e32b83a060c1aea6
Zakarias <zakarias.bw@gmail.com>
2018-03-01 23:16:52 +0000

    removed vim/bundle modified readme added install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+# Install packages
+#sudo apt-get update
+sudo apt-get install git -y
+sudo apt-get install vim -y
+
+
+# Setup dotfiles
+cd $HOME
+git clone --separate-git-dir=$HOME/.dotfiles https://github.com/ZakariasBW/dotfiles.git
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+dotfiles config --local status.showUntrackedFiles no
+echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
+rm -r $HOME/dotfiles
+
+# Inport all dotfiles
+dotfiles checkout .
+
+# Setup vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+
+
+
