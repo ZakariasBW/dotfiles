787d14db6a45992c8e64fa3fc86d99d9e83b93fe
Zakarias <zakarias.bw@gmail.com>
2018-03-02 00:10:09 +0000

    WIP

--- install.sh
+++ install.sh
@@ -10,13 +10,14 @@ sudo apt-get install vim -y
 cd $HOME
 git clone --separate-git-dir=$HOME/.dotfiles https://github.com/ZakariasBW/dotfiles.git
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+DOTFILES=usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME
 echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
-dotfiles config --local status.showUntrackedFiles no
+DOTFILES config --local status.showUntrackedFiles no
 rm -r $HOME/dotfiles
 
 # Import dotfiles
-dotfiles checkout .tmux*
-dotfiles checkout .vim*
+DOTFILES checkout .tmux*
+DOTFILES checkout .vim*
 
 # Setup vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
