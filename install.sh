d2f8d2813a896301af3bcad12b125f39c1fe6c93
Zakarias <zakarias.bw@gmail.com>
2019-04-06 00:18:43 +0200

    updated install.sh with use vim as default editor

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ cd $HOME
 git clone --separate-git-dir=$HOME/.dotfiles https://github.com/ZakariasBW/dotfiles.git
 shopt -s expand_aliases
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
+echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"' >> $HOME/.bashrc
 dotfiles config --local status.showUntrackedFiles no
 
 # Import dotfiles
@@ -21,6 +21,7 @@ dotfiles checkout .
 # Setup vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+echo $'# Set vim as defualt editor \nexport VISUAL=vim \nexport EDITOR="$VISUAL"' >> $HOME/.bashrc
 
 # Clean up
 rm -r dotfiles
